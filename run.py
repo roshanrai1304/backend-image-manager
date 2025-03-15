70cc8dec577e8de958c0ea0508c4465c5f00259b
Roshan Rai <roshan.rai@naturedots.com>
2025-03-15 16:40:17 +0530

    ssl certificate added

--- run.py
+++ run.py
@@ -1,11 +1,8 @@
 from app import create_app
-from app.config import Config
 
-key_file = Config.SSL_KEY_PATH
-cert_file = Config.SSL_CERT_PATH
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True, ssl_context=(cert_file, key_file))
+    app.run(host='0.0.0.0', port=5000)
 
